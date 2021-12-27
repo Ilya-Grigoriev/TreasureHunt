d8eecc37a578f2da7fc519286186bcd7047443b5
ilia0 <ilia092006grig@gmail.com>
2021-12-27 20:41:44 +0300

    Добавлены классы карты, игрока, созданы необходимые для работоспособности данных классов функции

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 import pygame
 
-size = width, height = 500, 500
+size = width, height = 700, 500
 screen = pygame.display.set_mode(size)
 player = None
 
@@ -29,6 +29,49 @@ def load_image(name, colorkey=None):
     return image
 
 
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+tile_images = {
+    'wall': load_image('wall.jpg'),
+    'floor': load_image('floor.png')
+}
+player_image = load_image('player_down.png')
+
+tile_width = tile_height = 32
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('floor', x, y)
+            elif level[y][x] == '*':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+    return new_player, x, y
+
+
 def load_level(filename):
     filename = "data/" + filename
     try:
@@ -41,19 +84,23 @@ def load_level(filename):
         terminate()
 
 
+level = load_level('first_level.txt')
+player, level_x, level_y = generate_level(level)
+
+
 def start_screen():
     fon = pygame.transform.scale(load_image('start_screen.png'), size)
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 80)
     string_rendered = font.render('Поиск', 1, pygame.Color('chocolate1'))
-    screen.blit(string_rendered, (165, 165))
+    screen.blit(string_rendered, (265, 165))
     string_rendered = font.render('сокровищ', 1, pygame.Color('chocolate1'))
-    screen.blit(string_rendered, (110, 215))
+    screen.blit(string_rendered, (210, 215))
     font = pygame.font.Font(None, 20)
     string_rendered = font.render('Создатель игры:', 1, pygame.Color('chocolate1'))
-    screen.blit(string_rendered, (195, 395))
+    screen.blit(string_rendered, (295, 395))
     string_rendered = font.render('Григорьев Илья', 1, pygame.Color('chocolate1'))
-    screen.blit(string_rendered, (198, 415))
+    screen.blit(string_rendered, (298, 415))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -70,4 +117,7 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             terminate()
+    screen.fill(pygame.Color((84, 55, 64)))
+    tiles_group.draw(screen)
+    player_group.draw(screen)
     pygame.display.flip()
