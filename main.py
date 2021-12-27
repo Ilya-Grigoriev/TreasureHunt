4f10e75ba619c8cb8c8c64404db818ad4146e51c
ilia0 <ilia092006grig@gmail.com>
2021-12-27 19:29:08 +0300

    init

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+import pygame
+
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+player = None
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    try:
+        with open(filename, 'r') as mapFile:
+            level_map = [line.strip() for line in mapFile]
+        max_width = max(map(len, level_map))
+        return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+    except FileNotFoundError:
+        print(f'Файл с именем {filename} не найден')
+        terminate()
+
+
+pygame.init()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminate()
+    pygame.display.flip()
